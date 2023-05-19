b4745b3336dbbc7c1df02a2e402a798a3e2fae02
judiciousmurich <murichjudicious45@gmail.com>
2023-05-19 21:05:07 +0300

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,10 @@ function SearchPanel() {
   }, [searchParam]);
 
   return (
-    <div className="search-panel">
-      <h1 className="description">Search for universities by country</h1>
-      <div className="input-btn">
+    <>
+
+    <div className="input-btn">
+      <h1 className="description desc1"> <span className="first-letter">s</span>earch for <span className="first-letter">u</span>niversities by <span className="first-letter">c</span>ountry</h1>
       <input
         type="text"
         placeholder="Enter a country name"
@@ -31,16 +32,20 @@ function SearchPanel() {
         onChange={(event) => setSearchParam(event.target.value)}
       />
       <button onClick={resetSearch}>Reset</button>
-
+      <h1 className="description desc2">Number of universities:{universities.length}</h1>
       </div>
-      <h1>Universities in countries are:{universities.length}</h1>
+     
+    <div className="search-panel">
+      
+      
       <ul>
         {universities.map((university) => (
           <li className="list" key={university.name}>
             <a href={university.web_pages[0]} target="_blank" rel="noreferrer">
-              {university.name}
+              
 
             </a>
+            <p>{university.name}</p>
             <p>Domains:{university.domains[0]}</p>
             <p>Website: <a href="">{university.web_pages[0]}</a></p>
             <p>Country:{university.country}</p>
@@ -48,6 +53,7 @@ function SearchPanel() {
         ))}
       </ul>
     </div>
+    </>
   );
 }
 
